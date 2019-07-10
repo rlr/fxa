17cc2d1bb01135ca156fc3089146121d25f59042
Lauren Zugai <lauren@zugai.com>
2019-07-10 18:39:13 -0500

    Add copy, begin modifying delete_account controller (WIP)

--- packages/fxa-content-server/app/scripts/views/settings/delete_account.js
+++ packages/fxa-content-server/app/scripts/views/settings/delete_account.js
@@ -10,6 +10,7 @@ import ServiceMixin from '../mixins/settings-panel-mixin';
 import Session from '../../lib/session';
 import SettingsPanelMixin from '../mixins/service-mixin';
 import Template from 'templates/settings/delete_account.mustache';
+import AttachedClients from '../../models/attached-clients';
 
 const t = msg => msg;
 
@@ -18,10 +19,31 @@ var View = FormView.extend({
   className: 'delete-account',
   viewName: 'settings.delete-account',
 
-  setInitialContext(context) {
-    context.set('email', this.getSignedInAccount().get('email'));
+  initialize(options) {
+    this._attachedClients = options.attachedClients;
+
+    if (!this._attachedClients) {
+      this._attachedClients = new AttachedClients([], {
+        notifier: options.notifier,
+      });
+    }
   },
 
+  _fetchAttachedClients() {
+    const start = Date.now();
+    return this._attachedClients.fetchClients(this.user).then(() => {
+      this.logFlowEvent(`timing.clients.fetch.${Date.now() - start}`);
+    });
+  },
+
+  setInitialContext(context) {
+    context.set({
+      email: this.getSignedInAccount().get('email'),
+      clients: this._attachedClients.toJSON(),
+    });
+  },
+
+  // to do: create deleteAccount method, move things from submit into it
   submit() {
     var account = this.getSignedInAccount();
     var password = this.getElementValue('.password');
