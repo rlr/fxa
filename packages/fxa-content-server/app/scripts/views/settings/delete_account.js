84e5726da5f0ea2e76269765037ca27b7bb2852c
Lauren Zugai <lauren@zugai.com>
2019-07-16 13:15:30 -0500

    add _fetchActiveSubscriptions

--- packages/fxa-content-server/app/scripts/views/settings/delete_account.js
+++ packages/fxa-content-server/app/scripts/views/settings/delete_account.js
@@ -10,7 +10,7 @@ import ServiceMixin from '../mixins/settings-panel-mixin';
 import Session from '../../lib/session';
 import SettingsPanelMixin from '../mixins/service-mixin';
 import Template from 'templates/settings/delete_account.mustache';
-import AttachedClients from '../../models/attached-clients';
+// import AttachedClients from '../../models/attached-clients';
 
 const t = msg => msg;
 
@@ -23,48 +23,60 @@ var View = FormView.extend({
   viewName: 'settings.delete-account',
 
   initialize(options) {
-    this._attachedClients = options.attachedClients;
+    // this._attachedClients = options.attachedClients;
+    // this._activeSubscriptions = options.activeSubscriptions;
 
-    if (!this._attachedClients) {
-      this._attachedClients = new AttachedClients([], {
-        notifier: options.notifier,
-      });
-    }
+    // if (!this._attachedClients) {
+    //   this._attachedClients = new AttachedClients([], {
+    //     notifier: options.notifier,
+    //   });
+    // }
+    this._activeSubscriptions = [];
   },
 
   setInitialContext(context) {
+    // still up in the air: do I display anything from 'clients'? May need other
+    // methods from clients.js if so (like _formatAccessTimeAndScope)
+
     context.set({
       email: this.getSignedInAccount().get('email'),
-      clients: this._attachedClients.toJSON(),
+      // clients: this._attachedClients.toJSON(),
       isPanelOpen: this.isPanelOpen(),
+      subscriptions: this._activeSubscriptions,
     });
   },
 
   openPanel() {
-    console.log('in openPanel');
     this.logViewEvent('open');
     this.$el.find(UNIT_DETAILS).hide();
     this.$el.find(LOADING_INDICATOR_BUTTON).show();
 
-    // returns a promise - but, we should be using getActiveSubscriptions
-    return this._fetchAttachedClients();
-    // return this.validateAndSubmit(null, {
-    //   artificialDelay: View.MIN_REFRESH_INDICATOR_MS,
-    // });
+    // return account.fetchActiveSubscriptions().then(subs => this.render());
+    // return this._fetchAttachedClients().then(() => this.render());
+
+    return Promise.all([
+      // this._fetchAttachedClients(),
+      this._fetchActiveSubscriptions(),
+    ]).then(() => this.render());
+  },
+
+  _fetchActiveSubscriptions() {
+    const account = this.getSignedInAccount();
+    const start = Date.now();
+    return account.fetchActiveSubscriptions().then(() => {
+      // do I need to this up anywhere else or does this create `timing.subscriptions`?
+      this.logFlowEvent(`timing.subscriptions.fetch.${Date.now() - start}`);
+    });
   },
 
   _fetchAttachedClients() {
     const start = Date.now();
-    // console.log(this._attachedClients.fetchClients(this.user).then(() => {
-    //   this.logFlowEvent(`timing.clients.fetch.${Date.now() - start}`);
-    // }));
     return this._attachedClients.fetchClients(this.user).then(() => {
       this.logFlowEvent(`timing.clients.fetch.${Date.now() - start}`);
     });
   },
 
   submit() {
-    console.log('in submit');
     var account = this.getSignedInAccount();
     var password = this.getElementValue('.password');
 
