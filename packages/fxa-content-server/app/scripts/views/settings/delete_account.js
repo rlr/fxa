808a0b4d804bb1ed9f452a8b670391bc97234e6c
Lauren Zugai <lauren@zugai.com>
2019-07-17 15:14:48 -0500

    Add validation for checkboxes, start subscriptions with empty array

--- packages/fxa-content-server/app/scripts/views/settings/delete_account.js
+++ packages/fxa-content-server/app/scripts/views/settings/delete_account.js
@@ -16,7 +16,9 @@ import { CLIENT_TYPE_WEB_SESSION } from '../../lib/constants';
 const t = msg => msg;
 
 const LOADING_INDICATOR_BUTTON = '.settings-button.settings-unit-loading';
+const DELETE_ACCOUNT_BUTTON = '.delete-account-button';
 const UNIT_DETAILS = '.settings-unit-details';
+const CHECKBOXES = '.delete-account-checkbox';
 
 var View = FormView.extend({
   template: Template,
@@ -25,14 +27,12 @@ var View = FormView.extend({
 
   initialize(options) {
     this._attachedClients = options.attachedClients;
-    this._activeSubscriptions = options.activeSubscriptions;
-
     if (!this._attachedClients) {
       this._attachedClients = new AttachedClients([], {
         notifier: options.notifier,
       });
     }
-    this._activeSubscriptions = ['test'];
+    this._activeSubscriptions = [];
   },
 
   _formatTitleAndScope(items) {
@@ -65,6 +65,10 @@ var View = FormView.extend({
     });
   },
 
+  events: {
+    'click .delete-account-checkbox': '_toggleEnableSubmit',
+  },
+
   openPanel() {
     this.logViewEvent('open');
     this.$el.find(UNIT_DETAILS).hide();
@@ -80,7 +84,6 @@ var View = FormView.extend({
     const account = this.getSignedInAccount();
     const start = Date.now();
     return account.fetchActiveSubscriptions().then(subscriptions => {
-      // do I need to this up anywhere else or does this create `timing.subscriptions`?
       this.logFlowEvent(`timing.subscriptions.fetch.${Date.now() - start}`);
       this._activeSubscriptions = subscriptions;
     });
@@ -91,6 +94,18 @@ var View = FormView.extend({
     return this._attachedClients.fetchClients(this.user).then(() => {
       this.logFlowEvent(`timing.clients.fetch.${Date.now() - start}`);
     });
+  },
+
+  _toggleEnableSubmit() {
+    if (this._allCheckboxesAreChecked()) {
+      this.$(DELETE_ACCOUNT_BUTTON).attr('disabled', false);
+    } else {
+      this.$(DELETE_ACCOUNT_BUTTON).attr('disabled', true);
+    }
+  },
+
+  _allCheckboxesAreChecked() {
+    return this.$(`${CHECKBOXES}:checked`).length === this.$(CHECKBOXES).length;
   },
 
   submit() {
